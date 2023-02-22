d3b41d45570e52c9d4791dd199abb6e94b4a4a8d
Mehmet Emin Ak <mak44162@gmail.com>
2023-02-22 16:43:36 +0100

    Initial Commit

---
+++ SwiftBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBank
+//
+//  Created by Macbook Air on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
